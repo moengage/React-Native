b0e7ebbca625d2aebe6e8a31592bedf3cb671fac
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2025-05-20 13:42:15 +0530

    MOEN-38239: XML based initialisation support

--- sdk/core/android/src/main/java/com/moengage/react/MoEInitializer.kt
+++ sdk/core/android/src/main/java/com/moengage/react/MoEInitializer.kt
@@ -7,6 +7,7 @@ import com.moengage.core.internal.logger.Logger
 import com.moengage.core.internal.model.IntegrationMeta
 import com.moengage.core.model.SdkState
 import com.moengage.plugin.base.internal.PluginInitializer
+import android.app.Application
 
 
 /**
@@ -55,5 +56,32 @@ object MoEInitializer {
         }
     }
 
-
+    /**
+     * Initialize SDK using file based configuration.
+     *
+     * Note: While using this function to intialize the SDK, make sure you have configured all the
+     * required configuration in the xml as resource value
+     */
+    @JvmOverloads
+    fun initializeDefaultInstance(
+        application: Application,
+        lifecycleAwareCallbackEnabled: Boolean = false,
+        sdkState: SdkState? = null,
+    ) {
+        try {
+            Logger.print { "$tag initialize(): Initialising MoEngage SDK" }
+            GlobalCache.lifecycleAwareCallbackEnabled = lifecycleAwareCallbackEnabled
+            PluginInitializer.initialize(
+                application = application,
+                integrationMeta = IntegrationMeta(
+                    INTEGRATION_TYPE,
+                    BuildConfig.MOENGAGE_REACT_LIBRARY_VERSION
+                ),
+                sdkState = sdkState
+            )
+            Logger.print { "$tag initialize(): " }
+        } catch (t: Throwable) {
+            Logger.print(LogLevel.ERROR, t) { "$tag initialize(): " }
+        }
+    }
 }
