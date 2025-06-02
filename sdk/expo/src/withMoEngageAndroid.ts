e8ee4ed9b754ca311345745e2761a8d6b72c2bf8
msoumya-engg-sdk <soumya.mahunt@moengage.com>
2025-06-02 11:58:52 +0530

    MOEN-38847: Added expo config interface

--- sdk/expo/src/withMoEngageAndroid.ts
+++ sdk/expo/src/withMoEngageAndroid.ts
@@ -1,6 +1,8 @@
 import { withAndroidManifest, ConfigPlugin } from '@expo/config-plugins';
+import { MoEngagePluginProps } from './types';
 
-const withMoEngageAndroid: ConfigPlugin = (config) => {
+const withMoEngageAndroid: ConfigPlugin<MoEngagePluginProps> = (config, props) => {
+  console.log(props)
   return withAndroidManifest(config, async (androidManifest) => {
     return androidManifest;
   });
