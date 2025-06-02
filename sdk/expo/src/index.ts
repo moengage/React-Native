46d9f3bd7c6eacfc4a7b0093caa03480d0365174
msoumya-engg-sdk <soumya.mahunt@moengage.com>
2025-06-02 11:58:52 +0530

    MOEN-38847: Added expo config interface

--- sdk/expo/src/index.ts
+++ sdk/expo/src/index.ts
@@ -1,11 +1,49 @@
-import { ConfigPlugin } from '@expo/config-plugins';
+import { ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
 import withMoEngageAndroid from './withMoEngageAndroid';
 import withMoEngageIos from './withMoEngageIos';
+import { MoEngagePluginProps } from './types';
 
-const withMoEngage: ConfigPlugin = (config) => {
-  config = withMoEngageAndroid(config);
-  config = withMoEngageIos(config);
+const defaultProps: MoEngagePluginProps = {
+  android: {
+    disableMoEngageDefaultBackupFile: false,
+    configFilePath: 'assets/moengage/android_initilisation_config.xml',
+  },
+  apple: {
+    configFilePath: 'assets/moengage/MoEngage-Config.plist',
+    pushNotificationImpressionTrackingEnabled: true,
+    richPushNotificationEnabled: false,
+    pushTemplatesEnabled: false,
+    deviceTriggerEnabled: false,
+    liveActivityTargetPath: '',
+  },
+};
+
+/**
+ * Configure MoEngage SDK in your Expo app
+ */
+const withMoEngage: ConfigPlugin<MoEngagePluginProps | undefined> = (config, props) => {
+  // Apply default values or handle undefined props
+  const pluginProps: MoEngagePluginProps = props ? {
+    ...defaultProps,
+    ...props,
+    android: {
+      ...defaultProps.android,
+      ...props.android,
+    },
+    apple: {
+      ...defaultProps.apple,
+      ...props.apple,
+    },
+  } : defaultProps
+  config = withMoEngageAndroid(config, pluginProps);
+  config = withMoEngageIos(config, pluginProps);
   return config;
 };
 
-export default withMoEngage;
+const pkg = require('../package.json');
+
+export default createRunOncePlugin(
+  withMoEngage,
+  pkg.name,
+  pkg.version
+);
---
+++ sdk/expo/src/types.ts
@@ -0,0 +1,82 @@
+/**
+ * MoEngage Android configuration options
+ */
+export interface MoEngageAndroidConfig {
+  /**
+   * Path to the Android configuration XML file
+   * @default "assets/moengage/android_initilisation_config.xml"
+   */
+  configFilePath: string;
+
+  /**
+   * Path to the small notification icon
+   */
+  smallIconPath?: string;
+
+  /**
+   * Path to the large notification icon
+   */
+  largeIconPath?: string;
+
+  /**
+   * Whether to disable MoEngage default backup file
+   * @default false
+   */
+  disableMoEngageDefaultBackupFile?: boolean;
+}
+
+/**
+ * MoEngage iOS configuration options
+ */
+export interface MoEngageIosConfig {
+  /**
+   * Path to the MoEngage configuration plist file.
+   * The data in this plist file is added to the MoEngage key in application's Info.plist
+   * @default "assets/moengage/MoEngage-Config.plist"
+   */
+  configFilePath: string;
+
+  /**
+   * Whether to enable push notification impression tracking
+   * @default true
+   */
+  pushNotificationImpressionTrackingEnabled: boolean;
+
+  /**
+   * Whether to enable rich push notifications
+   * @default false
+   */
+  richPushNotificationEnabled: boolean;
+
+  /**
+   * Whether to enable push templates
+   * @default false
+   */
+  pushTemplatesEnabled: boolean;
+
+  /**
+   * Whether to enable device triggers
+   * @default false
+   */
+  deviceTriggerEnabled: boolean;
+
+  /**
+   * Path to the Live Activity target
+   */
+  liveActivityTargetPath?: string;
+}
+
+/**
+ * MoEngage plugin configuration
+ */
+export interface MoEngagePluginProps {
+  /**
+   * Android-specific configuration options
+   */
+  android: MoEngageAndroidConfig;
+
+  /**
+   * iOS-specific configuration options
+   */
+  apple: MoEngageIosConfig;
+}
