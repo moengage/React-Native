54338018edce5cc0c90fef1c3abd36e1755bc6be
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2025-06-03 12:00:05 +0530

    MOEN-38659: Adding peer dependency for core module

--- sdk/expo/src/withMoEngageAndroid.ts
+++ sdk/expo/src/withMoEngageAndroid.ts
@@ -1,8 +1,9 @@
 import { withAndroidManifest, ConfigPlugin } from '@expo/config-plugins';
 import { MoEngagePluginProps } from './types';
+import { MoEngageLogger } from 'react-native-moengage';
 
 const withMoEngageAndroid: ConfigPlugin<MoEngagePluginProps> = (config, props) => {
-  console.log(props)
+  MoEngageLogger.debug('withMoEngageAndroid', props);
   return withAndroidManifest(config, async (androidManifest) => {
     return androidManifest;
   });
