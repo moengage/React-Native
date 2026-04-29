eecd19fb25e07558031b34ce7280089253d3c0bd
msoumya-engg-sdk <soumya.mahunt@moengage.com>
2026-04-29 10:22:03 +0530

    MOEN-44202: Fixed iOS sampleapp

--- SampleApp/ios/SampleApp/AppDelegate.h
+++ SampleApp/ios/SampleApp/AppDelegate.h
@@ -1,6 +1,10 @@
-#import <RCTAppDelegate.h>
+#import <React-RCTAppDelegate/RCTReactNativeFactory.h>
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate : RCTAppDelegate
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) RCTReactNativeFactory *reactNativeFactory;
 
 @end
--- SampleApp/ios/SampleApp/AppDelegate.mm
+++ SampleApp/ios/SampleApp/AppDelegate.mm
@@ -1,33 +1,16 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <React-RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 #import <ReactNativeMoEngage/MoEngageInitializer.h>
 #import <ReactNativeMoEngage/MoEngageReactSDKInitializationConfig.h>
 #import <MoEngageSDK/MoEngageSDK.h>
 
-@implementation AppDelegate
+@interface ReactNativeDelegate : RCTDefaultReactNativeFactoryDelegate
+@end
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  self.moduleName = @"SampleApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
-  [MoEngageSDKCore.sharedInstance enableAllLogs];
-
-  MoEngageSDKConfig* sdkConfig = [[MoEngageSDKConfig alloc] initWithAppId:@"YOUR APP ID" dataCenter: MoEngageDataCenterData_center_01];
-  sdkConfig.appGroupID = @"group.com.alphadevs.MoEngage.NotificationServices";
-  sdkConfig.consoleLogConfig = [[MoEngageConsoleLogConfig alloc] initWithIsLoggingEnabled:true loglevel:MoEngageLoggerTypeVerbose];
-
-  // Initialization with code
-  // MoEngageReactSDKInitializationConfig *initConfig = [[MoEngageReactSDKInitializationConfig alloc] initWithSdkConfig:sdkConfig];
-  // [[MoEngageInitializer sharedInstance] initializeInstance:initConfig];
-
-  // File based initialization without swizzling
-  // [[MoEngageInitializer sharedInstance] initializeDefaultInstanceWithAdditionalReactConfig:[[MoEngageReactSDKDefaultInitializationConfig alloc] init]];
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
+@implementation ReactNativeDelegate
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
@@ -45,4 +28,79 @@
 
 @end
 
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+@property (nonatomic, strong) UIWindow *window;
+@end
 
+@implementation SceneDelegate
+
+- (void)scene:(UIScene *)scene
+    willConnectToSession:(UISceneSession *)session
+                 options:(UISceneConnectionOptions *)connectionOptions
+{
+  if (![scene isKindOfClass:[UIWindowScene class]]) return;
+  UIWindowScene *windowScene = (UIWindowScene *)scene;
+  AppDelegate *appDelegate = (AppDelegate *)UIApplication.sharedApplication.delegate;
+  UIWindow *window = [[UIWindow alloc] initWithWindowScene:windowScene];
+  [appDelegate.reactNativeFactory startReactNativeWithModuleName:@"SampleApp"
+                                                        inWindow:window
+                                               initialProperties:@{}
+                                                   launchOptions:nil];
+  self.window = window;
+  appDelegate.window = window;
+}
+
+- (void)scene:(UIScene *)scene openURLContexts:(NSSet<UIOpenURLContext *> *)urlContexts
+{
+  for (UIOpenURLContext *context in urlContexts) {
+    [MoEngageSDKAnalytics.sharedInstance processURL:context.URL];
+  }
+}
+
+- (void)scene:(UIScene *)scene continueUserActivity:(NSUserActivity *)userActivity
+{
+  [MoEngageSDKAnalytics.sharedInstance processURL:userActivity.webpageURL];
+}
+
+@end
+
+@interface AppDelegate ()
+@property (nonatomic, strong) ReactNativeDelegate *reactNativeDelegate;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  UNUserNotificationCenter.currentNotificationCenter.delegate = self;
+  self.reactNativeDelegate = [ReactNativeDelegate new];
+  self.reactNativeDelegate.dependencyProvider = [RCTAppDependencyProvider new];
+  self.reactNativeFactory = [[RCTReactNativeFactory alloc] initWithDelegate:self.reactNativeDelegate];
+
+  [MoEngageSDKCore.sharedInstance enableAllLogs];
+
+  // Initialization with code
+  // MoEngageSDKConfig *sdkConfig = [[MoEngageSDKConfig alloc] initWithAppId:@"YOUR APP ID" dataCenter:MoEngageDataCenterData_center_01];
+  // sdkConfig.appGroupID = @"group.com.alphadevs.MoEngage.NotificationServices";
+  // sdkConfig.consoleLogConfig = [[MoEngageConsoleLogConfig alloc] initWithIsLoggingEnabled:true loglevel:MoEngageLoggerTypeVerbose];
+  // MoEngageReactSDKInitializationConfig *initConfig = [[MoEngageReactSDKInitializationConfig alloc] initWithSdkConfig:sdkConfig];
+  // [[MoEngageInitializer sharedInstance] initializeInstance:initConfig];
+
+  // File based initialization without swizzling
+  // [[MoEngageInitializer sharedInstance] initializeDefaultInstanceWithAdditionalReactConfig:[[MoEngageReactSDKDefaultInitializationConfig alloc] init]];
+
+  return YES;
+}
+
+- (UISceneConfiguration *)application:(UIApplication *)application
+    configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession
+                                   options:(UISceneConnectionOptions *)options
+{
+  UISceneConfiguration *config = [[UISceneConfiguration alloc] initWithName:@"Default Configuration"
+                                                                sessionRole:connectingSceneSession.role];
+  config.delegateClass = SceneDelegate.class;
+  return config;
+}
+
+@end
