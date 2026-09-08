50faacb75c2d12e13b9efa51933ffb114294daf5
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2026-09-08 12:08:17 +0530

    Removing iOS Changes

--- sdk/personalize/ios/MoEngagePersonalizeBridge.h
+++ sdk/personalize/ios/MoEngagePersonalizeBridge.h
@@ -1,7 +1,15 @@
 // MoEngagePersonalizeBridge.h
 
 #import <React/RCTBridgeModule.h>
-#import <NativeMoEngagePersonalizeSpec/NativeMoEngagePersonalizeSpec.h>
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <NativeMoEngagePersonalizeSpec/NativeMoEngagePersonalizeSpec.h>
+#endif
+
+#ifdef RCT_NEW_ARCH_ENABLED
 @interface MoEngagePersonalizeBridge : NSObject <NativeMoEngagePersonalizeSpec>
 @end
+#else
+@interface MoEngagePersonalizeBridge : NSObject <RCTBridgeModule>
+@end
+#endif
