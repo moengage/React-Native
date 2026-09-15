0688523782369d5084e1b4a5925f0750871e8ee2
msoumya-engg-sdk <soumya.mahunt@moengage.com>
2026-09-15 17:24:26 +0530

    MOEN-47644: Removed old architecture support

--- SpmSampleApp/ios/SpmSampleApp/AppDelegate.swift
+++ SpmSampleApp/ios/SpmSampleApp/AppDelegate.swift
@@ -110,10 +110,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
-
   override func bundleURL() -> URL? {
 #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
